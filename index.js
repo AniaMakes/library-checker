560b0e5b33cc8890e06abb38806f85b480d10a9b
Ania <31079643+AniaMakes@users.noreply.github.com>
2026-01-20 12:19:51 +0000

    tidy up city of london fetcher

--- index.js
+++ index.js
@@ -2,54 +2,41 @@ import puppeteer from "puppeteer";
 import fetchHardcoverTBR from "./hardcover-api/fetchToBeReadList.js";
 import tidyHardcoverOutput from "./hardcover-api/tidyHardcoverOutput.js";
 
+// const books = await fetchHardcoverTBR();
+// const tidyHardcoverData = tidyHardcoverOutput(books);
+
+const bookList = [
+	{ title: "Katabasis", authorSurname: "Kuang" },
+	{ title: "The binding", authorSurname: "Collins" },
+];
+
+// const bookList = tidyHardcoverData;
+
+// Open browser window / page
 const browser = await puppeteer.launch({ headless: false });
 const page = await browser.newPage();
 
 async function checkLibrary() {
-	const books = await fetchHardcoverTBR();
-	const tidyHardcoverData = tidyHardcoverOutput(books);
-
 	// Navigate the page to a URL.
 	await page.goto("https://col.ent.sirsidynix.net.uk/client/en_GB/default/#");
 
 	// Set screen size.
 	await page.setViewport({ width: 1080, height: 1024 });
 
-	let allBookListOutput = [];
-	const title = "Katabasis";
-	const authorSurname = "Kuang";
-
-	//   const bookList = [
-	//     { title: "Katabasis", authorSurname: "Kuang" },
-	//     { title: "The binding", authorSurname: "Collins" },
-	//   ];
-
-	const bookList = tidyHardcoverData;
-	// const oneItem = bookList[0]
-
 	for (let i = 0; i < bookList.length; i++) {
-		// =======
+		// inputs the search term into the search box
 		const oneItem = bookList[i];
 		const searchBox = `[id="q"]`;
 		await page.locator(searchBox).fill(oneItem.title);
 		await page.keyboard.press("Enter");
 
+		// allows console logs to surface for debugging
 		page.on("console", (consoleObj) => console.log(consoleObj.text()));
 
-		const resultsBox = '[id="results_wrapper"]';
-
-		// find class results_cell
-		// const data = await page.evaluate(() => {
-		// 	let divs = [...document.querySelectorAll('.results_cell')];
-		// 	const output = divs.map(div => {
-		// 		return div.innerText;
-		// 	})
-		// 	console.log(output)
-		// })
-
 		let results;
-		// ---
+
 		try {
+			// find out which selector surfaces first
 			// https://github.com/puppeteer/puppeteer/issues/709
 			const raceSelectors = (page, selectors) => {
 				return Promise.race(
@@ -68,18 +55,17 @@ async function checkLibrary() {
 				".no_results_wrapper",
 			]);
 
+			// if there are no results, report that back, then go to the next book
 			if (selector === ".no_results_wrapper") {
 				bookList[i].barbican = "no match found";
 				continue;
 			}
 
-			//   await page.waitForSelector(".results_cell");
+			// wait for secondary info to fetch and make sure network is idle before proceeding
 			await page.waitForSelector(".detailItemTable");
 			await page.waitForNetworkIdle();
 
-			// if no results "No results found in Search Results.\nSwitch to next category."
-			// in searchResults_wrapper
-
+			// gets result "cards"
 			results = await page.$$eval(
 				".results_cell",
 				function (results, oneItem) {
@@ -88,6 +74,7 @@ async function checkLibrary() {
 					results.forEach(async function (result) {
 						const innerText = result.innerText;
 
+						// only run the scraper on results that match the author as the search results can unrelated
 						if (innerText.includes(oneItem.authorSurname)) {
 							const id = result.id.replace("results_cell", "");
 
@@ -110,6 +97,7 @@ async function checkLibrary() {
 
 							finalOutput = clean3SearchOutput;
 
+							// if the format is a physical book, get info which library it's at and the section name
 							if (innerText.includes("Format:  Books")) {
 								const tableId = `#detailItemTableCust${id}`;
 								const tableItself = document.querySelector(".detailItemTable");
@@ -117,7 +105,6 @@ async function checkLibrary() {
 								const rows = tableItself.getElementsByClassName(
 									"detailItemsTableRow",
 								);
-								// console.log("rows", rows);
 
 								let bookHardcopies = "";
 								for (let i = 0; i < rows.length; i++) {
@@ -133,12 +120,10 @@ async function checkLibrary() {
 
 									const output = `${libraryName[0].innerText.replace("Searching...", "")} ${shelfName[0].innerText} ${statusName[0].innerText.replace("Searching...", "")}`;
 									bookHardcopies += `\n${output}`;
-									// console.log("output", output);
 								}
 
 								finalOutput = clean3SearchOutput.concat(bookHardcopies);
 							}
-							// console.log(finalOutput);
 							searchResults.push(finalOutput);
 						}
 					});
@@ -154,33 +139,15 @@ async function checkLibrary() {
 		}
 
 		bookList[i].barbican = results;
+
+		// be kind, don't spam them with requests, wait 5s before each
 		const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
 		await sleep(5000);
-
-		// const singleBookOutput = {
-		//   ...oneItem,
-		//   results,
-		// };
-
-		// allBookListOutput.push(singleBookOutput);
-		// console.log('================')
-		// console.log("results", results);
-		// console.log('================')
-
-		// =======
 	}
 
 	console.log(bookList);
-	// ---
 
-	// WORKS for single item
-	// await page.waitForSelector('.results_cell')
-	// const result = await page.$eval('.results_cell', e => e.innerText);
-
-	// console.log(result);
 	await browser.close();
-
-	// ---
 }
 
-// checkLibrary();
+checkLibrary();
---
+++ libraries/UK/cityOfLondon.js
@@ -0,0 +1,8 @@
+// Barbican Library, Shoe Lane Library, Artizan Street Library
+
+/**
+ * Scrapes data from the City Of London library catalogue.
+ * @param page - Puppeteer page.
+ * @param bookList - Array of objects
+ */
+export default async function checkCityOfLondon(page, bookList) { }
